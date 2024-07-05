c0cce85a3bc23bbec766857f6cece419c9a8673e
kunal12kumar <23cs3035@rgipt.ac.in>
2024-07-05 16:37:29 +0530

    updating with the code  to send the data to python file

--- emotion.js
+++ emotion.js
@@ -1,20 +1,30 @@
-const form=document.querySelector(".inputb");
-const submit=document.querySelector("#Submit");
-const searchbox=document.querySelector("#searchbox");
+const form = document.querySelector(".inputb");
+const submit = document.querySelector("#Submit");
+const searchbox = document.querySelector("#searchbox");
 
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const text=searchbox.value;
+    const text = searchbox.value;
     console.log(text);
+    const data = { key: `${text}` };
 
-    const object={
-        'key':text};
-    console.log(object);
-    $.ajax({
-        
+    fetch('http://localhost:5000/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
-    
+
+
 });
+
+
 
